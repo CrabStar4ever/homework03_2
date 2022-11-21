efe09a4316c057916e74f5338d044f02000e990f
CrabStar4ever <>
2022-11-21 15:16:24 +0800

    optimize imports

--- 3/springbucks-client/src/main/java/com/learning/springbucksclient/SpringbucksClientApplication.java
+++ 3/springbucks-client/src/main/java/com/learning/springbucksclient/SpringbucksClientApplication.java
@@ -1,11 +1,9 @@
 package com.learning.springbucksclient;
 
-import com.learning.springbucksclient.config.RestTemplateConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
